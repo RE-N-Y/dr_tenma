265366ecd1b91a070de4694a8869d2f196daef7a
NC <sangwulee2@gmail.com>
2020-07-09 16:26:41 +0900

    aws amplify set-up

---
+++ src/graphql/mutations.ts
@@ -0,0 +1,79 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createStudent = /* GraphQL */ `
+  mutation CreateStudent(
+    $input: CreateStudentInput!
+    $condition: ModelStudentConditionInput
+  ) {
+    createStudent(input: $input, condition: $condition) {
+      id
+      name
+      email
+      records {
+        Fever
+        Coughing
+        Breathing
+        SoreThroat
+        Allergies
+        BodyAches
+        note
+        createdAt
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const updateStudent = /* GraphQL */ `
+  mutation UpdateStudent(
+    $input: UpdateStudentInput!
+    $condition: ModelStudentConditionInput
+  ) {
+    updateStudent(input: $input, condition: $condition) {
+      id
+      name
+      email
+      records {
+        Fever
+        Coughing
+        Breathing
+        SoreThroat
+        Allergies
+        BodyAches
+        note
+        createdAt
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const deleteStudent = /* GraphQL */ `
+  mutation DeleteStudent(
+    $input: DeleteStudentInput!
+    $condition: ModelStudentConditionInput
+  ) {
+    deleteStudent(input: $input, condition: $condition) {
+      id
+      name
+      email
+      records {
+        Fever
+        Coughing
+        Breathing
+        SoreThroat
+        Allergies
+        BodyAches
+        note
+        createdAt
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
