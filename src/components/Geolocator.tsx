fc6e8bc40c91691fcced5c457e9b66ea158fcbd7
NC <sangwulee2@gmail.com>
2020-07-13 15:47:00 +0900

    enable high accuracy

--- src/components/Geolocator.tsx
+++ src/components/Geolocator.tsx
@@ -17,6 +17,9 @@ const Geolocator: React.FC = () => {
         },
         (error) => {
           console.log(error);
+        },
+        {
+          enableHighAccuracy: true,
         }
       );
     } else {
